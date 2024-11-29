20905677336638ca745fe86b721eda159de99564
Babygrill ☕️ <139216162+FerrousInk@users.noreply.github.com>
2024-11-29 22:59:10 +0100

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,1 +1,14 @@
-paru -S xdg-user-dirs adw-gtk-theme adwaita-qt5 adwaita-qt6 qt5ct qt6ct hyprland hyprpaper waybar swaync rofi-wayland grim slurp polkit-gnome nautilus alacritty zsh ttf-jetbrains-mono-nerd brightnessctl wl-clipboard xclip nwg-look xdg-desktop-portal-gtk xdg-desktop-portal-hyprland oh-my-posh-bin hyprlock hypridle zoxide nwg-displays carla calf lsp-plugins pavucontrol qt5-wayland
+echo -----------------
+echo   Dependencies
+echo -----------------
+paru -S xdg-user-dirs adw-gtk-theme adwaita-qt5 adwaita-qt6 qt5ct qt6ct hyprland hyprpaper waybar swaync rofi-wayland grim slurp polkit-gnome nautilus alacritty ttf-jetbrains-mono-nerd brightnessctl wl-clipboard xclip nwg-look xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprlock hypridle nwg-displays qt5-wayland
+
+echo -----------------
+echo     ZSH Deps
+echo -----------------
+paru -S oh-my-posh-bin zsh zoxide
+
+echo -----------------
+echo    Good Audio
+echo -----------------
+paru -S carla calf lsp-plugins pavucontrol
