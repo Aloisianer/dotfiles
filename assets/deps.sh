16a769e2d79cab73519c2db35613a30c905df28c
FerrousInk <moaugustin@proton.me>
2024-12-14 15:24:25 +0100

    Fixed Wallpaper, Removed Dependeny

--- assets/deps.sh
+++ assets/deps.sh
@@ -1,7 +1,7 @@
 echo -----------------
 echo   Dependencies
 echo -----------------
-paru -S xdg-user-dirs adw-gtk-theme adwaita-qt5 adwaita-qt6 qt5ct qt6ct hyprland hyprpaper waybar swaync rofi-wayland grim slurp polkit-gnome nautilus alacritty ttf-jetbrains-mono-nerd brightnessctl wl-clipboard xclip nwg-look xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprlock hypridle nwg-displays qt5-wayland gamescope xorg-xhost
+paru -S xdg-user-dirs adw-gtk-theme adwaita-qt5 adwaita-qt6 qt5ct qt6ct hyprland hyprpaper waybar swaync rofi-wayland grim slurp polkit-gnome nautilus alacritty ttf-jetbrains-mono-nerd brightnessctl wl-clipboard xclip nwg-look xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprlock hypridle qt5-wayland gamescope xorg-xhost
 
 echo -----------------
 echo     ZSH Deps
