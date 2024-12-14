749fa727aad9856a19096201240d6d4dd80c3cc2
FerrousInk <moaugustin@proton.me>
2024-12-14 14:58:51 +0100

    Gud Sutff, + Gaming Goods

---
+++ apps/waybar/restart.sh
@@ -0,0 +1,2 @@
+pkill waybar
+hyprctl dispatch exec "waybar -c ~/.config/hypr/apps/waybar/config.jsonc -s ~/.config/hypr/apps/waybar/style.css"
---
+++ assets/deps.sh
@@ -0,0 +1,14 @@
+echo -----------------
+echo   Dependencies
+echo -----------------
+paru -S xdg-user-dirs adw-gtk-theme adwaita-qt5 adwaita-qt6 qt5ct qt6ct hyprland hyprpaper waybar swaync rofi-wayland grim slurp polkit-gnome nautilus alacritty ttf-jetbrains-mono-nerd brightnessctl wl-clipboard xclip nwg-look xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprlock hypridle nwg-displays qt5-wayland gamescope xorg-xhost
+
+echo -----------------
+echo     ZSH Deps
+echo -----------------
+paru -S oh-my-posh-bin zsh zoxide
+
+echo -----------------
+echo    Good Audio
+echo -----------------
+paru -S carla calf lsp-plugins pavucontrol
