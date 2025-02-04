6d956c8c7c2eed215bdf16cf5ccc6fee586dd17b
FerrousInk <moaugustin@proton.me>
2025-02-04 12:26:22 +0100

    Switched to kitty cuz i found a ssh fix

--- deps/amd.sh
+++ deps/amd.sh
@@ -1,1 +1,1 @@
-paru -S lib32-vulkan-radeon vulkan-radeon lib32-mesa mesa
+paru -S lib32-vulkan-radeon vulkan-radeon lib32-mesa mesa lact-libadwaita
--- deps/terminal.sh
+++ deps/terminal.sh
@@ -1,1 +1,1 @@
-paru -S starship zsh zoxide bat eza zsh-completions
+paru -S starship zsh zoxide bat eza zsh-completions btop diskonaut fastfetch kew ugrep
