b2d21d56c4989050782f9cbac3ea0ec725666086
FerrousInk <moaugustin@proton.me>
2025-02-04 14:03:57 +0100

    Kitty update

--- deps/terminal.sh
+++ deps/terminal.sh
@@ -1,1 +1,1 @@
-paru -S starship zsh zoxide bat eza zsh-completions btop diskonaut fastfetch kew ugrep
+paru -S starship zsh zoxide bat eza zsh-completions btop diskonaut fastfetch kew ugrep ttf-jetbrains-mono ttf-jetbrains-mono-nerd
