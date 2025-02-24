5c9db0b0fff7d4ba477c82782dd27c8aa992cac0
Alois~ <moaugustin@proton.me>
2025-02-24 15:40:29 +0100

    Did cool stuff

--- deps/terminal.sh
+++ deps/terminal.sh
@@ -1,1 +1,1 @@
-paru -S starship zsh zoxide bat eza zsh-completions btop diskonaut fastfetch kew ugrep ttf-jetbrains-mono ttf-jetbrains-mono-nerd
+paru -S starship nushell zoxide bat eza zsh-completions btop diskonaut fastfetch kew ugrep ttf-jetbrains-mono ttf-jetbrains-mono-nerd sshpass
